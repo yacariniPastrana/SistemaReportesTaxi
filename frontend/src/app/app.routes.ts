89c611d1e1fa75ce332e2cc002a83da9946d13a2
yacariniPastrana <yacarini.pastrana@gmail.comgit config --global user.email yacarini.pastrana@gmail.com>
2026-02-27 04:48:08 -0500

    feat: tarea exitosa login con backend y creacion del dashboard

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
 
 export const routes: Routes = [
     { path: ``, redirectTo: `login`, pathMatch: `full` },
-    { path: `login`, component: LoginComponent }
+    { path: `login`, component: LoginComponent },
+    { path: `dashboard`, component: DashboardComponent }
 ];
